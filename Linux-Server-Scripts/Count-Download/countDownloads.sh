aaa7fc7aa37a797f2c6b83eba125c9c0d00b3530
Seifeldin Sabry <ismailseifeldin54@gmail.com>
2022-01-07 16:38:10 +0000

    added comments to the script

--- Linux-Server-Scripts/Count-Download/countDownloads.sh
+++ Linux-Server-Scripts/Count-Download/countDownloads.sh
@@ -44,6 +44,8 @@ else
 #  sed -i "/$(date +%d\ %b  | sed 's%/0%/%g' | tr '[:upper:]' '[:lower:]')/d" "${htmlLocation}"
 #fi
 
+
+#these sed commands delete lines that contain These 'Patterns/Strings'.
   sed -i '/Total/d' "${htmlLocation}"
   sed -i '/This script took/d' "${htmlLocation}"
   sed -i '/Last updated on/d' "${htmlLocation}"
@@ -57,6 +59,14 @@ fi
 
 
 # shellcheck disable=SC2002
+#for the total download I'm doing 3 things:
+#1. finding the <p> elements
+#2. finding the numbers (amount of downloads for all days)
+#3. adding them in an implicit for loop (awk += $4)
+#the '$' is for the field. A typical line in the script looks like this: <p>07 jan - x downloads.
+# the '-' is the 3rd field and the number is the 4th so im just stacking up the numbers
+#finally, to set the variable, the PRINT outputs the sum into the variable
+
 totalDownload=$(cat "${htmlLocation}" | grep "<p>" | grep -E "[^0-9][0-9]+" | awk '{ SUM += $4;} END { print SUM;}')
 echo -e "<p>Total: ${totalDownload} downloads.</p>" >> "${htmlLocation}"
 
